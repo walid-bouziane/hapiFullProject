15143fb4eb7a121256f1b7e2292062975b84f01c
waiss67 <baqui.waiss@gmail.com>
2018-09-15 21:40:21 +0200

    first querry

--- myAPIhapi/README.md
+++ myAPIhapi/README.md
@@ -1,7 +1,56 @@
-start mangodb
+start project
 
 ```
 sudo service mongod start
+npm start
+```
+
+mongo command
+
+show all collection
+
+```
+  show dbs
+```
+
+import csv
+
+```
+  $ mongoimport -d mydb -c things --type csv --file locations.csv --headerline
+```
+
+find in collection
+
+```
+  db.crime.find()
+```
+
+show databases
+
+```
+  show dbs
 ```
 
 https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+
+https://mongoosejs.com/docs/guide.html
+https://docs.mongodb.com/manual/reference/method/db.collection.find/
+
+### cours etna config file
+
+```
+  npm i config --save
+  mkdir config
+  touch default.json
+```
+
+edit config/default.json
+
+```
+{
+  "api": {
+    "port": 3000,
+    "host": "localhost"
+  }
+}
+```
--- myAPIhapi/server.js
+++ myAPIhapi/server.js
@@ -1,19 +1,87 @@
 "use strict";
 
 const Hapi = require("hapi");
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/test");
+const mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+mongoose.connect(
+  "mongodb://localhost:27017/hapiProject",
+  {
+    useNewUrlParser: true
+  }
+);
+var connexionInstance = mongoose.connection;
+connexionInstance.on("error", console.error.bind(console, "connection error:"));
+
+// const config = require("config");
+// const server = Hapi.server(config.get(api));
 
 const server = Hapi.server({
   host: "localhost",
   port: 8000
 });
 
+// { "_id" : ObjectId("5b993e321c6f0735b81a3572"), "compnos" : 152038722, "naturecode" : "MVA", "incident_type_description" : "MVAcc", "main_crimecode" : "MVAcc", "reptdistrict" : "E13",
+// "reportingarea" : 0, "fromdate" : "2015-05-12T06:00:00", "weapontype" : "Other", "shooting" : "False", "domestic" : "False", "shift" : "Last", "year" : 2015, "month" : 5,
+// "day_week" : "Tuesday", "ucrpart" : "Other", "x" : 760592.1395, "y" : 2934324.064, "streetname" : "HYDE\n PARK AV", "xstreetname" : "WELD HILL ST", "location" : "(42.29928636, -71.11399955)" }
+var MyModel = mongoose.model(
+  "crime",
+  new Schema({
+    _id: String,
+    compnos: String,
+    naturecode: String,
+    MVA: String,
+    incident_type_description: String,
+    main_crimecode: String,
+    reptdistrict: String,
+    reportingarea: String,
+    main_crimecode: String,
+    fromdate: Date,
+    weapontype: String,
+    shooting: Boolean,
+    domestic: Boolean,
+    shift: String,
+    year: Number,
+    month: Number,
+    day_week: String,
+    ucrpart: String,
+    x: Number,
+    y: Number,
+    streetname: String,
+    xstreetname: String,
+    location: String
+  })
+);
+// var MyModel2 = mongoose.model(
+//   "crimes",
+//   new Schema({
+//     compnos: 152038722
+//   })
+// );
+
+// MyModel2.find();
+
 server.route({
   method: "GET",
   path: "/hello",
   handler: function(request, h) {
-    return "hello world";
+    var nameApp = "baka hapi js";
+
+    console.log("we are not connected");
+
+    mongoose.model(
+      "question",
+      new Schema(
+        { _id: String, location: String, weapontype: String },
+        { collection: "crime" }
+      )
+    );
+    var questions = mongoose.model("question");
+    questions.find({}, function(err, data) {
+      console.log(err, data, data.length);
+      return data;
+    });
+
+    // return nameApp;
   }
 });
 
@@ -29,3 +97,9 @@ async function start() {
 }
 
 start();
+
+// pour avoir la base de données mangoos
+// db.once("open", function () {
+//   // we're connected!
+//   console.log("open");
+// });
